70fbdbea3ea56655942e5bb80afbd457b946c77d
Rocky528 <98113805+Rocky528@users.noreply.github.com>
2023-05-08 09:50:32 +0300

    changed_count2
    changed_count2
    
    Co-Authored-By: Game | Backend Engineer <9028177+Backendeng@users.noreply.github.com>

--- src/app/dashboard/Dashboard.js
+++ src/app/dashboard/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 // import { increment, decrement } from '../../reducers/counterSlice'
-import { AAAO1, AAAO2, EEEO1, MMMO1, CCCO1, EEEO3 } from '../../reducers/InputSlice'
+import { AAAO1, AAAO2, EEEO1, MMMO1, CCCO1, EEEO3, EEEO2, CCCO2, GGGO1, IIIO1, JJJO1 } from '../../reducers/InputSlice'
 import FormTag from '../Component/FormTag';
 import FormTag1 from '../Component/FormTag1';
 import GaugeChart from "react-gauge-chart";
@@ -35,6 +35,7 @@ const Dashboard = () => {
   const handleEEEO1 = (evt) => {
     setEO1(evt.target.value);
   }
+
   //DDDO1 (CO1/(EO1-EO3)/MO1)*EO1*MO1
   const [DO1, setDO1] = useState(0);
   const [CO1, setCO1] = useState(0);
@@ -45,6 +46,71 @@ const Dashboard = () => {
   const handleEEEO3 = (evt) => {
     setEO3(evt.target.value);
   }
+
+  //DDDO2 (CO1/(EO1-EO3)/MO1)
+  const [DO2, setDO2] = useState(0);
+
+  //HHHO2 EO1*EO2*MO1
+  const [HO2, setHO2] = useState(0);
+  const [EO2, setEO2] = useState(0);
+  const handleEEEO2 = (evt) => {
+    setEO2(evt.target.value);
+  }
+
+  //FFFO1 HO2-(EO3/EO1*HO2)
+  const [FO1, setFO1] = useState(0);
+
+  //FFFO2 EO1-(CO1/EO2)/MO1
+  const [FO2, setFO2] = useState(0);
+
+  //FFFO3 EO1+(EO1*CO2/HO2)
+  const [FO3, setFO3] = useState(0);
+  const [CO2, setCO2] = useState(0);
+  const handleCCCO2 = (evt) => {
+    setCO2(evt.target.value);
+  }
+
+  //GGGO2 (EO2*GO1-EO2*EO1)*MO1
+  const [GO2, setGO2] = useState(0);
+  const [GO1, setGO1] = useState(0);
+  const handleGGGO1 = (evt) => {
+    setGO1(evt.target.value);
+  }
+
+  //HHHO1 GO2/HO2
+  const [HO1, setHO1] = useState(0);
+
+  //IIIO2 HO2*IO1*0.01*-1
+  const [IO2, setIO2] = useState(0);
+  const [IO1, setIO1] = useState(0);
+  const handleIIIO1 = (evt) => {
+    setIO1(evt.target.value);
+  }
+
+  //JJJO2 EO1*EO2*JO1*MO1
+  const [JO2, setJO2] = useState(0);
+  const [JO1, setJO1] = useState(0);
+  const handleJJJO1 = (evt) => {
+    setJO1(evt.target.value);
+  }
+
+  //KKKO1 EO1-IO1*EO1
+  const [KO1, setKO1] = useState(0);
+
+  //KKKO2 EO1+(EO1*JO2/HO2)
+  const [KO2, setKO2] = useState(0);
+
+  //HHHO3 GO2/CO1
+  const [HO3, setHO3] = useState(0);
+
+  //LLLO1 EO3-EO1
+
+  //LLLO2 GO1-EO1
+
+  //LLLO3 GO1-EO3
+
+  //LLLO4 GO2-100
+
   useEffect(() => {
     // Update the document title using the browser API
     dispatch(AAAO1(AO1));
@@ -53,9 +119,25 @@ const Dashboard = () => {
     dispatch(MMMO1(MO1));
     dispatch(CCCO1(CO1));
     dispatch(EEEO3(EO3));
+    dispatch(EEEO2(EO2));
+    dispatch(CCCO2(CO2));
+    dispatch(GGGO1(GO1));
+    dispatch(IIIO1(IO1));
+    dispatch(JJJO1(JO1));
     setBO1(AO1 * 0.01 * AO2);
     setBO2(BO1 / EO1 / MO1);
     setDO1((CO1 / (EO1 - EO3) / MO1) * EO1 * MO1);
+    setDO2((CO1 / (EO1 - EO3) / MO1));
+    setHO2(EO1 * EO2 * MO1);
+    setFO1(HO2 - (EO3 / EO1 * HO2));
+    setFO2(EO1 - (CO1 / EO2) / MO1);
+    setFO3(EO1 + (EO1 * CO2 / HO2));
+    setGO2((EO2 * GO1 - EO2 * EO1) * MO1);
+    setHO1(GO2 / HO2);
+    setIO2(HO2 * IO1 * 0.01 * -1);
+    setJO2(EO1 * EO2 * JO1 * MO1);
+    setKO1(EO1 - IO1 * EO1);
+    setKO2(EO1 + (EO1 * JO2 / HO2));
   },);
 
   return (
@@ -102,7 +184,7 @@ const Dashboard = () => {
                   </div>
                   <div className="row mt-1">
                     <div className="col-12">
-                      <FormTag color="input_green" title="CCCO2($)" val="$" />
+                      <FormTag color="input_green" title="CCCO2($)" onChange={handleCCCO2} val="$" />
                     </div>
                   </div>
                   <div className="row mt-1">
@@ -133,7 +215,7 @@ const Dashboard = () => {
                           arcsLength={[0.2, 0.1, 0.2, 0.5]}
                         />
                       </div>
-                      <h4 className="text-center mt-4">HHHO3 = 102.93 = C20/C9</h4>
+                      <h4 className="text-center mt-4">HHHO3 = {HO3}= GGGO2/CCCO1</h4>
                     </div>
                   </div>
                 </div>
@@ -150,34 +232,34 @@ const Dashboard = () => {
                   <h4 style={{ color: "#CBCBE2" }}>Price Position Trading</h4>
                   <div className="row mt-5">
                     <div className="col-6">
-                      <FormTag color="red" title="FFFO1" value="$ 546" />
+                      <FormTag color="red" title="FFFO1" value={"$" + FO1} />
                     </div>
                     <div className="col-6">
-                      <FormTag color="blue" title="DDDO2" value="$ 286" />
-                    </div>
-                  </div>
-                  <div className="row mt-4">
-                    <div className="col-6">
-                      <FormTag color="red" title="FFFO2" value="$ 546" />
-                    </div>
-                    <div className="col-6">
-                      <FormTag color="green" title="FFFO3" value="$ 286" />
+                      <FormTag color="blue" title="DDDO2" value={"$" + DO2} />
                     </div>
                   </div>
                   <div className="row mt-4">
                     <div className="col-6">
-                      <FormTag color="yellow" title="HHHO2" value="$ 546" />
+                      <FormTag color="red" title="FFFO2" value={"$ " + FO2} />
                     </div>
                     <div className="col-6">
-                      <FormTag color="green" title="GGGO2" value="$ 286" />
+                      <FormTag color="green" title="FFFO3" value={"$ " + FO3} />
                     </div>
                   </div>
                   <div className="row mt-4">
                     <div className="col-6">
-                      <FormTag color="yellow" title="DDDO1" value={"$" + DO1} />
+                      <FormTag color="yellow" title="HHHO2" value={"$ " + HO2} />
                     </div>
                     <div className="col-6">
-                      <FormTag color="green" title="HHHO1" value="$ 286" />
+                      <FormTag color="green" title="GGGO2" value={"$ " + GO2} />
+                    </div>
+                  </div>
+                  <div className="row mt-4">
+                    <div className="col-6">
+                      <FormTag color="yellow" title="DDDO1" value={"$ " + DO1} />
+                    </div>
+                    <div className="col-6">
+                      <FormTag color="green" title="HHHO1" value={"$ " + HO1} />
                     </div>
                   </div>
                 </div>
@@ -196,7 +278,7 @@ const Dashboard = () => {
                   </div>
                   <div className="row mt-1">
                     <div className="col-12">
-                      <FormTag color="input_blue" val="$" title="EEEO2($)" />
+                      <FormTag color="input_blue" val="$" title="EEEO2($)" onChange={handleEEEO2} />
                     </div>
                   </div>
                   <div className="row mt-1">
@@ -206,7 +288,7 @@ const Dashboard = () => {
                   </div>
                   <div className="row mt-1">
                     <div className="col-12">
-                      <FormTag color="input_green" val="$" title="GGGO1($)" />
+                      <FormTag color="input_green" val="$" title="GGGO1($)" onChange={handleGGGO1} />
                     </div>
                   </div>
                   <div class="row mt-4">
@@ -230,26 +312,26 @@ const Dashboard = () => {
                   <h4 style={{ color: "#CBCBE2" }}>Percentitle Trading</h4>
                   <div className="row mt-5">
                     <div className="col-6">
-                      <FormTag1 color="red" title="HHHO1" value="1K" />
+                      <FormTag1 color="red" title="HHHO1" value={HO1 + " K"} />
                     </div>
                     <div className="col-6">
-                      <FormTag1 color="blue" title="MMMO1" value="42.9" />
-                    </div>
-                  </div>
-                  <div className="row mt-4">
-                    <div className="col-6">
-                      <FormTag1 color="red" title="IIIO2" value="$ 45.5" />
-                    </div>
-                    <div className="col-6">
-                      <FormTag1 color="green" title="JJJO2" value="$ 42.9" />
+                      <FormTag1 color="blue" title="MMMO1" value={MO1} />
                     </div>
                   </div>
                   <div className="row mt-4">
                     <div className="col-6">
-                      <FormTag1 color="red" title="KKKO1" value="$ 5.4" />
+                      <FormTag1 color="red" title="IIIO2" value={"$ " + IO2} />
                     </div>
                     <div className="col-6">
-                      <FormTag1 color="green" title="KKKO2" value="$ 444.4" />
+                      <FormTag1 color="green" title="JJJO2" value={"$ " + JO2} />
+                    </div>
+                  </div>
+                  <div className="row mt-4">
+                    <div className="col-6">
+                      <FormTag1 color="red" title="KKKO1" value={"$ " + KO1} />
+                    </div>
+                    <div className="col-6">
+                      <FormTag1 color="green" title="KKKO2" value={"$ " + KO2} />
                     </div>
                   </div>
                 </div>
@@ -263,22 +345,22 @@ const Dashboard = () => {
                   <h4 style={{ color: "#CBCBE2" }}>Percentitle Trading</h4>
                   <div className="row mt-4">
                     <div className="col-12">
-                      <FormTag color="input_yellow" val="$" title="EEEO1($)" />
+                      <FormTag color="input_yellow" val="$" title="EEEO1($)" onChange={handleEEEO1} />
                     </div>
                   </div>
                   <div className="row mt-1">
                     <div className="col-12">
-                      <FormTag color="input_blue" val="$" title="EEEO2($)" />
+                      <FormTag color="input_blue" val="$" title="EEEO2($)" onChange={handleEEEO2} />
                     </div>
                   </div>
                   <div className="row mt-1">
                     <div className="col-12">
-                      <FormTag color="input_red" val="%" title="IIIO1($)" />
+                      <FormTag color="input_red" val="%" title="IIIO1($)" onChange={handleIIIO1} />
                     </div>
                   </div>
                   <div className="row mt-1">
                     <div className="col-12">
-                      <FormTag color="input_green" val="%" title="JJJO1($)" />
+                      <FormTag color="input_green" val="%" title="JJJO1($)" onChange={handleJJJO1} />
                     </div>
                   </div>
                   <div class="row mt-4">
--- src/reducers/InputSlice.js
+++ src/reducers/InputSlice.js
@@ -8,6 +8,10 @@ export const InputSlice = createSlice({
     MMMO1: 0,
     CCCO1: 0,
     EEEO3: 0,
+    EEEO2: 0,
+    CCCO2: 0,
+    IIIO1: 0,
+    JJJO1: 0,
   },
   reducers: {
     AAAO1: (state, action) => {
@@ -26,9 +30,24 @@ export const InputSlice = createSlice({
       state.CCCO1 = action.payload
     },
     EEEO3: (state, action) => {
-      state.MMMO1 = action.payload
-    }
+      state.EEEO3 = action.payload
+    },
+    EEEO2: (state, action) => {
+      state.EEEO2 = action.payload
+    },
+    CCCO2: (state, action) => {
+      state.CCCO2 = action.payload
+    },
+    GGGO1: (state, action) => {
+      state.GGGO1 = action.payload
+    },
+    IIIO1: (state, action) => {
+      state.IIIO1 = action.payload
+    },
+    JJJO1: (state, action) => {
+      state.JJJO1 = action.payload
+    },
   },
 })
-export const { AAAO1, AAAO2, EEEO1, MMMO1, CCCO1, EEEO3 } = InputSlice.actions
+export const { AAAO1, AAAO2, EEEO1, MMMO1, CCCO1, EEEO3, EEEO2, CCCO2, GGGO1, IIIO1, JJJO1 } = InputSlice.actions
 export default InputSlice.reducer
