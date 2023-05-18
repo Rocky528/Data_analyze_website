6ba726fe5588e1aedf4de88937707a1352129ceb
Rocky528 <98113805+Rocky528@users.noreply.github.com>
2023-05-18 03:12:39 -0400

    com517.4.!.12
    com517.4.!.12
    
    Co-Authored-By: Game | Backend Engineer <9028177+Backendeng@users.noreply.github.com>

--- src/app/dashboard/Tab2.js
+++ src/app/dashboard/Tab2.js
@@ -4,7 +4,6 @@ import FormTag1 from '../Component/FormTag1';
 import FormTag2 from '../Component/FormTag2';
 import ButtonTag from '../Component/ButtonTag';
 import GaugeChart from "react-gauge-chart";
-import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux'
 import TableInput from '../Component/TableInput';
 import './style.css';
