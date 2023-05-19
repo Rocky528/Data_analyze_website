21418207284c01bf70a390163c337f685bf92459
Rocky528 <98113805+Rocky528@users.noreply.github.com>
2023-05-19 02:25:29 -0400

    com 5.19 2.25
    com 5.19 2.25

--- src/app/Component/TableInput.js
+++ src/app/Component/TableInput.js
@@ -1,14 +1,21 @@
 
 import React from 'react';
 import { NumericFormat } from 'react-number-format'
+import { Form } from 'react-bootstrap';
 
-function NumberInput() {
+function NumberInput(props) {
+    console.log(props.type)
     return (
-        <NumericFormat thousandSeparator={true}
-            style={{ color: "white", backgroundColor: "#3B4758", borderColor: "#3B4758" }}
+        props.type === 'text' ? <Form.Control
+            type="text"
+            style={{ color: "white", backgroundColor: "#3B4758" }}
             className="form-control"
-            aria-describedby="basic-addon1"
+        /> : < NumericFormat thousandSeparator={true}
+            style={{ color: "white", backgroundColor: "#3B4758", borderColor: "#3B4758" }
+            }
+            className="form-control"
         />
+
     );
 }
 
